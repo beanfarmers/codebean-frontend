603acf814ac6e0a71172e5c3b6cfa03a65f9c629
litsynp <nocte55is@gmail.com>
2022-08-04 11:05:42 +0900

    feat: add GitHub logo

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import GitHubLogo from '@src/components/GitHubLogo';
+
 const Footer = () => (
   <footer className='flex flex-1 justify-center items-center py-5 px-0 bg-gray-800'>
     <a
@@ -9,6 +11,9 @@ const Footer = () => (
       className='flex justify-center items-center text-gray-200 space-x-2'
     >
       <p>CodeBean by</p>
+      <p>
+        <GitHubLogo />
+      </p>
       <p>litsynp</p>
     </a>
   </footer>
