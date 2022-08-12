752f5fe9a39898d63dd3b00ad219c15f5895d24d
litsynp <nocte55is@gmail.com>
2022-08-12 20:37:48 +0900

    fix: fix nav bar link to main page

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -6,7 +6,7 @@ const NavBar = () => (
   <div className='flex h-16 px-5 bg-gray-800'>
     <NavBarTextButton
       text='CodeBean'
-      url='/!'
+      url='/'
       className='my-auto text-2xl italic'
     />
     <div className='flex ml-12 space-x-10 font-bold'>
